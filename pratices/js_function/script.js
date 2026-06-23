1910516dae26b1b56f6daa5fdd72c837d3f63c81
Shubham Sharma <24f2001661@ds.study.iitm.ac.in>
2026-06-23 13:13:48 +0530

    js_23-06-2026

---
+++ pratices/js_function/script.js
@@ -0,0 +1,23 @@
+// function greet() {
+//     console.log("Hello");
+// }
+
+// greet();/
+
+// function add(a, b) {
+//     console.log(a + b);
+// }
+
+// add(5, 10);/
+
+function vote(name, age) {
+    console.log("Welcome, " + name);
+
+    if (age < 18) {
+        console.log("You are not eligible to vote");
+    } else {
+        console.log("You are eligible to vote");
+    }
+
+}
+vote("shub", 23);
